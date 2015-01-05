288f8e178fbd9d58e10bf8adae266be97a65adb0
Raul Jimenez <rauljmz@gmail.com>
2015-01-05 17:52:28 +0000

    2e add a controller rendering

---
+++ MVC8/Controllers/NavigationMenu.cs
@@ -0,0 +1,34 @@
+﻿using MVC8.Models;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC8.Controllers
+{
+    public class NavigationMenuController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            var homeItem = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+
+            var navigationItems = new List<NavigationItem>();
+
+            navigationItems.Add(new NavigationItem() { NavigationTitle = "Home", Url = "/" });
+
+           navigationItems.AddRange(homeItem.Children
+                .Where(i=> i.Template.Key == "product")
+                .Select(i=> new NavigationItem(){
+                    NavigationTitle = i.Name,
+                    Url = LinkManager.GetItemUrl(i)
+                }));
+     
+            
+            return View(navigationItems);
+        }
+    }
+}
---
+++ MVC8/Models/NavigationItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC8.Models
+{
+    public class NavigationItem
+    {
+        public string NavigationTitle { get; set; }
+        public string Url { get; set; }
+    }
+}
