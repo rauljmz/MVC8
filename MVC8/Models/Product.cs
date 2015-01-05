9fb520dddeea1ef3abd0a8ea954eb71320573bac
Raul Jimenez <rauljmz@gmail.com>
2015-01-05 17:16:01 +0000

    2d custom model

---
+++ MVC8/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC8.Models
+{
+    public class Product : RenderingModel
+    {
+        public HtmlString Title
+        {
+            get
+            {
+                return new HtmlString(FieldRenderer.Render(Item, "title"));
+            }
+        }
+        public HtmlString Description { get { return new HtmlString(FieldRenderer.Render(Item, "description")); } }
+        public HtmlString Image { get { return new HtmlString(FieldRenderer.Render(Item, "image")); } }
+
+
+
+    }
+}
