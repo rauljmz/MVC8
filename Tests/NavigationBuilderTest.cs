6dbc6e158446cbeea9288b4c08565061f63186b2
Kevin Obee <kevin.obee@gmail.com>
2015-01-15 23:12:53 +0800

    Theory test added to reduce test functions

--- Tests/NavigationBuilderTest.cs
+++ Tests/NavigationBuilderTest.cs
@@ -3,6 +3,7 @@ using MVC8.Business;
 using System.Linq;
 using Should;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Tests
 {
@@ -11,9 +12,7 @@ namespace Tests
         private readonly Mock<IItemRepository> _itemRepository;
         private readonly NavigationBuilder _navigationBuilder;
 
-        private readonly TestItem _child1;
-        private readonly TestItem _child2;
-        private readonly TestItem _homeItem;
+        private static TestItem _homeItem;
 
         public NavigationBuilderTest()
         {
@@ -23,15 +22,14 @@ namespace Tests
 
             _homeItem = new TestItem { Name = "Home", TemplateName = "Sample Item", Url = "/" };
 
-            _child1 = new TestItem { Name = "child1", TemplateName = "Sample Item", Url = "/child1" };
-            _child2 = new TestItem { Name = "child2", TemplateName = "Sample Item", Url = "/child2" };
-
+            var child1 = new TestItem { Name = "child1", TemplateName = "Sample Item", Url = "/child1" };
+            var child2 = new TestItem { Name = "child2", TemplateName = "Sample Item", Url = "/child2" };
 
             _itemRepository.Setup(r => r.GetItem("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}"))
                            .Returns(_homeItem);
 
             _itemRepository.Setup(r => r.GetChildren(_homeItem))
-                           .Returns(new[] { _child1, _child2 });
+                           .Returns(new[] { child1, child2 });
         }
 
         [Fact]
@@ -45,21 +43,15 @@ namespace Tests
                              .ShouldEqual(0);
         }
 
-        [Fact]
-        public void ReturnsAllFirstLevelChildren()
+        [Theory]
+        [InlineData("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}", "/")]
+        [InlineData("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}", "/child1")]
+        [InlineData("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}", "/child2")]
+        public void VerifyReturnedItems(string itemId, string returnedItemUrl)
         {
-            var result = _navigationBuilder.NavigationForItem("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}").ToArray();
-
-            result.Any(x => x.Url == _child1.Url).ShouldBeTrue();
-            result.Any(x => x.Url == _child2.Url).ShouldBeTrue();
-        }
-
-        [Fact]
-        public void ReturnsHomeItem()
-        {
-            _navigationBuilder.NavigationForItem("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}")
-                              .First()
-                              .Url.ShouldEqual(_homeItem.Url);
+            _navigationBuilder.NavigationForItem(itemId)
+                              .Any(x => x.Url == returnedItemUrl)
+                              .ShouldBeTrue();
         }
     }
 }
